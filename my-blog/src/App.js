14f8f989eda5582fd42f2145deb1c240c9fd800a
bashdemy <87253492+bashdemy@users.noreply.github.com>
2021-11-24 21:31:54 +1100

    Maria/main/optimize router (#1)
    * Add new routes.
    
    * Add id
    
    * Add css.

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -1,16 +1,23 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-
+import AboutPage from "./pages/AboutPage";
+import ArticlePage from "./pages/ArticlePage";
+import ArticlesList from "./pages/ArticlesList";
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path={"/"} exact component={HomePage} />
-        </Routes>
-      </Router>
-    </div>
+    <Router>
+      <div className="App">
+        <div id="page-body">
+          <Routes>
+            <Route path={"/"} exact element={<HomePage />} />
+            <Route path={"/about"} element={<AboutPage />} />
+            <Route path={"/articles-list"} exact element={<ArticlesList />} />
+            <Route path={"/article"} exact element={<ArticlePage />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 

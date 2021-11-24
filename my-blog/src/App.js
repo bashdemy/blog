b10d5987984e50f75ec6bf98f53ad038f6421626
bashdemy <bashdemy@gmail.com>
2021-11-24 21:29:04 +1100

    Add id

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -1,19 +1,21 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import AboutPage from "./pages/AboutPage"
-import ArticlePage from "./pages/ArticlePage"
-import ArticlesList from "./pages/ArticlesList"
+import AboutPage from "./pages/AboutPage";
+import ArticlePage from "./pages/ArticlePage";
+import ArticlesList from "./pages/ArticlesList";
 function App() {
   return (
     <Router>
       <div className="App">
-        <Routes>
-          <Route path={"/"} exact element={<HomePage/>} />
-          <Route path={"/about"}  element={<AboutPage/>} />
-          <Route path={"/articles-list"} exact element={<ArticlesList/>} />
-          <Route path={"/article"} exact element={<ArticlePage/>} />
-        </Routes>
+        <div id="page-body">
+          <Routes>
+            <Route path={"/"} exact element={<HomePage />} />
+            <Route path={"/about"} element={<AboutPage />} />
+            <Route path={"/articles-list"} exact element={<ArticlesList />} />
+            <Route path={"/article"} exact element={<ArticlePage />} />
+          </Routes>
+        </div>
       </div>
     </Router>
   );

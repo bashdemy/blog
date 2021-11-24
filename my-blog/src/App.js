8a89a77898984bbf2e66a69c52218688e470f87b
bashdemy <bashdemy@gmail.com>
2021-11-24 21:28:31 +1100

    Add new routes.

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -1,16 +1,21 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-
+import AboutPage from "./pages/AboutPage"
+import ArticlePage from "./pages/ArticlePage"
+import ArticlesList from "./pages/ArticlesList"
 function App() {
   return (
-    <div className="App">
-      <Router>
+    <Router>
+      <div className="App">
         <Routes>
-          <Route path={"/"} exact component={HomePage} />
+          <Route path={"/"} exact element={<HomePage/>} />
+          <Route path={"/about"}  element={<AboutPage/>} />
+          <Route path={"/articles-list"} exact element={<ArticlesList/>} />
+          <Route path={"/article"} exact element={<ArticlePage/>} />
         </Routes>
-      </Router>
-    </div>
+      </div>
+    </Router>
   );
 }
 

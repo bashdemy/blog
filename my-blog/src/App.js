37a134c97c404c5be4a09a3719c28e807126edb0
bashdemy <bashdemy@gmail.com>
2022-01-02 19:38:37 +1100

    Make Article List a modular component.

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import ArticlePage from "./pages/ArticlePage";
-import ArticlesList from "./pages/ArticlesList";
+import ArticlesListPage from "./pages/ArticlesListPage";
 import NavBar from "./NavBar";
 function App() {
   return (
@@ -14,7 +14,7 @@ function App() {
           <Routes>
             <Route path={"/"} exact element={<HomePage />} />
             <Route path={"/about"} element={<AboutPage />}/>
-            <Route path={"/articles-list"} exact element={<ArticlesList />} />
+            <Route path={"/articles-list"} exact element={<ArticlesListPage />} />
             <Route path={"/article/:name"} exact element={<ArticlePage />} />
           </Routes>
         </div>
--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -1,22 +1,29 @@
 import React from "react";
 import articleContent from "./article-content";
 import { useParams } from "react-router-dom";
+import ArticlesList from "../components/ArticlesList";
 const ArticlePage = () => {
 
   const { name } = useParams();
+
+  const otherArticles = articleContent.filter(article => article.name != name);
 
   const article = articleContent.find((article) => {
     return article.name == name;
   });
 
   if (!article) return <h1>Article does not exist!</h1>
+
+
   return (
     <>
       <h1>{article.title}</h1>
       {article.content.map((paragraph, key) => {
-        <p key={key}>{paragraph}</p>;
+        return <p key={key}>{paragraph}</p>
       })}
+      <ArticlesList articles={otherArticles}></ArticlesList>
     </>
+
   );
 };
 
